ea2c102b9045141d0befea3b32d332545bf6e31a
Guo, Junming <sjunmg@gmail.com>
2020-02-24 16:46:15 -0600

    add - section 2 - working on our form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { PostsService } from './../posts.service';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { PostsService } from "./../posts.service";
 
 @Component({
-  selector: 'app-post-create',
-  templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create-component.css']
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create-component.css"]
 })
 export class PostCreateComponent {
-  enteredTitle = '';
-  enteredContent = '';
+  enteredTitle = "";
+  enteredContent = "";
 
   constructor(public postsService: PostsService) {}
 
   onAddPost(form: NgForm) {
-    if (form.invalid) { return; }
+    if (form.invalid) {
+      return;
+    }
     this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
