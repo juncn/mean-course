c888bcf3cb3815191ca75c69840e5452a3a51d22
Guo, Junming <sjunmg@gmail.com>
2020-02-24 15:07:56 -0600

    add - section 2 - creating a post model

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -7,10 +8,5 @@ import { Component, Input } from '@angular/core';
 })
 
 export class PostListComponent {
-  // posts = [
-  //   { title: 'First post', content: 'This is the content of first post' },
-  //   { title: 'Second post', content: 'This is the content of second post' },
-  //   { title: 'Third post', content: 'This is the content of third post' }
-  // ]
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
 }
