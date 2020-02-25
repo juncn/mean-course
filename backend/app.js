466cdadcc68f5138ab26fb599bdb920e6da12f0b
Guo, Junming <sjunmg@gmail.com>
2020-02-25 14:07:11 -0600

    add - section 3 - fetching initial posts

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "adfasdfo14292",
+      title: "First server side post",
+      content: "This is post from the server side"
+    },
+    {
+      id: "adfaajfo14222",
+      title: "Second server side post",
+      content: "This is the second post from the server side"
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
