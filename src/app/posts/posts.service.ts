c383cfeb5b08aa77cbc75f6e10ea17a4381f9947
Guo, Junming <sjunmg@gmail.com>
2020-02-25 14:36:29 -0600

    add - section 3 - using the angular HTTP client

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private httpClient: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.httpClient.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdatedListener() {
@@ -17,6 +24,7 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {
+      id: null,
       title: title,
       content: content
     };
